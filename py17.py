7d9cab920f6944a2d29c7878fb9e4ee3845df634
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-26 22:42:49 +0530

    1.

---
+++ py17.py
@@ -0,0 +1,28 @@
+#return statement = a statement used within a function to send python value/object back to the caller!!**
+#                    ***and these value/objects known as function's return value!****
+
+#def multiply(number1,number2):
+#    result = number1 * number2
+#    return result
+#x = multiply(6,7)
+#y = multiply(3.8,4)
+#print(x)
+#print(y)
+
+#def multiply(a,b):
+#    r = a * b
+
+#x = multiply(2,4)
+#print(x)
+
+#def multiply(number1,number2):
+#    result = number1*number2
+#    return result
+
+#x = multiply(6,7)
+#print(x)
+
+#def multiply(a,b): #way of writting code in less lines!!
+#    return a*b
+#x = multiply(3,4)
+#print(x)
